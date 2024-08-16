1fd7bedc0419e0298f7c6aea090d6035a5174fc3
Srijan Nayak <srijan.nayak@capgemini.com>
2024-08-16 13:22:09 +0530

    create token type constants

--- token/token.go
+++ token/token.go
@@ -1,8 +1,34 @@
 package token
 
-type TokenType string
-
 type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 14124
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
