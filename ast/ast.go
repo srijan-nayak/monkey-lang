1c16e65159f83028f7e455e0064bc9075b029c3a
Srijan Nayak <srijannayak0@gmail.com>
2024-08-17 19:16:00 +0530

    create structs for representing let statements and identifiers

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/srijan-nayak/monkey-lang/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -24,3 +26,20 @@ func (p *Program) TokenLiteral() string {
 	}
 	return ""
 }
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
