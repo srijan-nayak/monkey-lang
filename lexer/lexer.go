d99ac3956bb8d562bb1eafe323647d9648292728
Srijan Nayak <srijannayak0@gmail.com>
2024-08-17 11:48:40 +0530

    add support for two-character opertors to lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -22,36 +22,67 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() != '=' {
+			tok = newToken(token.ASSIGN, l.ch)
+			break
+		}
+
+		ch := l.ch
+		l.readChar()
+		literal := string(ch) + string(l.ch)
+		tok = token.Token{Type: token.EQ, Literal: literal}
+
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
+
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
+
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() != '=' {
+			tok = newToken(token.BANG, l.ch)
+			break
+		}
+
+		ch := l.ch
+		l.readChar()
+		literal := string(ch) + string(l.ch)
+		tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+
 	case '*':
 		tok = newToken(token.ASTERISK, l.ch)
+
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
+
 	case '<':
 		tok = newToken(token.LT, l.ch)
+
 	case '>':
 		tok = newToken(token.GT, l.ch)
+
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
+
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
+
 	case '(':
 		tok = newToken(token.LPAREN, l.ch)
+
 	case ')':
 		tok = newToken(token.RPAREN, l.ch)
+
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
+
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
@@ -68,6 +99,14 @@ func (l *Lexer) NextToken() token.Token {
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
 
 func (l *Lexer) readChar() {
